bb75c59ef992edf0ee4802aa56ffc2f153e4e69d
Piyush Ghumelia <piyush.ghumelia@gmail.com>
2025-04-03 10:40:17 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,12 @@
+var1 = int(input("Enter the first number: "))
+var2 = int(input("Enter the second number "))
+
+a=var1+var2
+s=var1-var2
+m=var1*var2
+d=var1/var2
+
+print(f"Addition: {a}")
+print(f"Subtraction: {s}")
+print(f"Multiplication: {m}")
+print(f"Division: {d}")
